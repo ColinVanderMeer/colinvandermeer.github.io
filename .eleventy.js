e6519d300cd4e81ce8434019238bb897988b7a6e
ColinVanderMeer <colinvandermeer@gmail.com>
2025-02-10 08:22:57 -0500

    Fix main page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+
 export default function (eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyVitePlugin);
+    
     eleventyConfig.addPassthroughCopy({ "src/static": "/" });
 }
