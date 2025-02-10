463661f88f3dee94d9a9a56456a6dd3b87949e2e
ColinVanderMeer <colinvandermeer@gmail.com>
2025-02-10 13:13:43 -0500

    Add date display to article

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,22 @@
 import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
 
 export default function (eleventyConfig) {
+    eleventyConfig.addFilter("date_to_datetime", async (obj) => { // Thank you maia crimew for this code
+        if (!obj) {
+          return "";
+        }
+        let date = parseDate(obj);
+        date.setHours(24); // This fixes the date being off by one for some reason (smth smth localtime?)
+        return date.toDateString();
+    });
+
+    const parseDate = (str) => {
+        if (str instanceof Date) {
+          return str;
+        }
+        return Date.parse(str);
+    };
+
     eleventyConfig.addPassthroughCopy({ "src/static": "/public" });
     eleventyConfig.addPassthroughCopy({ "src/css": "/css/" }); // Extra fix so vite can optimize css
     
--- src/blog/testArticle.md
+++ src/blog/testArticle.md
@@ -2,7 +2,7 @@
 layout: article
 title: Using Eleventy to build a static site
 description: it's apparently illegal to run a blog and not write some sort of year-in-review list
-date: 2022-02-14
+date: 2025-02-11
 tags:
   - tag1
   - tag2
